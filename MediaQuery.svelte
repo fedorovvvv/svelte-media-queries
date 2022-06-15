7c8df7ae4b725d3aec737c29585a9579a093afba
NikitaPalmDev <nikitafedorov7@yandex.ru>
2022-06-15 20:37:54 +0300

    1.0.0

---
+++ MediaQuery.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onMount } from "svelte";
+
+    export let query
+    export let matches = false
+
+    const checkMatches = () => matches = window.matchMedia(query).matches
+    onMount(() => checkMatches)
+    $:checkMatches(query)
+</script>
+
+<svelte:window on:resize={checkMatches}/>
+<slot {matches}/>
