5a957c2be9aed997896196090520abb2652f1024
NikitaPalmDev <nikitafedorov7@yandex.ru>
2022-06-15 21:31:10 +0300

    fix: flat

--- MediaQuery.svelte
+++ MediaQuery.svelte
@@ -4,13 +4,13 @@
     export let query
     export let matches = false
 
-    const getMatches = (query) => window.matchMedia(query).matches
+    const getMatches = (query) => window.matchMedia(query ?? '').matches
 
     const checkMatches = () => {
         if (typeof query === 'string') {
             return matches = getMatches(query)
         }
-        return matches = query.flat(Infinity).map(getMatches)
+        return matches = query?.flat(Infinity).map(getMatches)
     }
     onMount(() => checkMatches)
     $:checkMatches(query)
