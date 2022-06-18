720ffc062969bf8c9dc68406e4873544303463c4
NikitaPalmDev <nikitafedorov7@yandex.ru>
2022-06-18 13:34:38 +0300

    move to svelte-kit package

---
+++ src/lib/components/MediaQuery.svelte
@@ -0,0 +1,38 @@
+<script context='module' lang='ts'>
+    export type Query = string
+    export type QueryAny = Query | Query[]
+
+    export type Matches = boolean
+    export type MatchesArray = Matches[]
+    export type MatchesAny = Matches | MatchesArray
+    
+</script>
+
+<script lang='ts'>
+    import { onMount } from "svelte";
+
+    export let query:QueryAny
+    export let matches:MatchesAny = false
+    export let matchesArray:MatchesArray = [false]
+
+    let mounted = false
+
+    const getMatches = (query:Query) => window.matchMedia(query).matches ?? false
+
+    const checkMatches = (query:QueryAny) => {
+        if (mounted) {
+            if (typeof query === 'string') {
+                return matches = getMatches(query)
+            }
+            return matches = matchesArray = query?.flat(Infinity).map(getMatches)
+        }
+    }
+    onMount(() => {
+        mounted = true
+        checkMatches(query)
+    })
+    $:checkMatches(query)
+</script>
+
+<svelte:window on:resize={() => checkMatches(query)}/>
+<slot {matches}/>
